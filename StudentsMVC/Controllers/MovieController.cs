f6b7f22c3d4a5af7932035efd171e921f550f6d4
Petr Zalizko <pzalizko@gmail.com>
2024-05-10 13:02:35 +0300

    Добавлены возможности Редактировать, Детали, Добавить фильм. При добавлении фильма либо добавляется картинка и нет деталей фильма в бд, либо есть детали но нет картинки.

--- StudentsMVC/Controllers/MovieController.cs
+++ StudentsMVC/Controllers/MovieController.cs
@@ -11,11 +11,15 @@ namespace StudentsMVC.Controllers
                 
         // Объявление контекста базы данных фильмов
         MovieContext _movieContext;
+
+		// IWebHostEnvironment предоставляет информацию об окружении, в котором запущено приложение
+		IWebHostEnvironment _webHostEnvironment;
                 
-        // Конструктор контроллера с инъекцией контекста базы данных фильмов
-        public MovieController(MovieContext movieContext)
+        // Внедряем ссылки через конструктор
+        public MovieController(MovieContext movieContext, IWebHostEnvironment webHostEnvironment)
         {
             _movieContext = movieContext;
+            _webHostEnvironment = webHostEnvironment;
         }
 
 		///////////////////////////////////////////////////////////////////////////////////////////
@@ -46,34 +50,125 @@ namespace StudentsMVC.Controllers
             return View(movie);
         }
 
-		///////////////////////////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////////////////////////
 
-		// Get запрос на Create
-		public IActionResult Create()
+        // Get запрос на Create
+
+        /*
+        public IActionResult Create()
+		{
+            return View(_movieContext.Movies.ToList());
+		}
+        */
+
+        [HttpGet]
+        public IActionResult Create()
         {
             return View();
         }
-                
-        // Post запрос на Create
-        [HttpPost] // получение данных от клиента, при нажатии СОХРАНИТЬ данные сохраняются в БД
-        [ValidateAntiForgeryToken] // проверка данных перед тем как они отправятся в базу данных
 
-        // Bind - инициализация полей объекта, происходит напрямую через форму html
-        public async Task<IActionResult> Create([Bind("Id,Title,Director,Genre,ReleaseYear,PosterPath,Description")] Movie movie)
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Id,Title,Director,Genre,ReleaseYear,Description")] Movie movie, IFormFile posterFile)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
+                if (posterFile != null && posterFile.Length > 0)
+                {
+                    string fileName = Path.GetFileName(posterFile.FileName);
+                    string filePath = Path.Combine(_webHostEnvironment.WebRootPath, "Image", fileName);
+
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await posterFile.CopyToAsync(fileStream);
+                    }
+                    movie.PosterPath = "/Image/" + fileName;
+                }
                 _movieContext.Add(movie);
-                await _movieContext.SaveChangesAsync(); // синхронизация с БД
-                return RedirectToAction(nameof(Index));
+                await _movieContext.SaveChangesAsync();
+                return RedirectToAction("Index");
             }
-			return View(movie);
-		}
+            return View(movie);
+        }
+        
 
-		///////////////////////////////////////////////////////////////////////////////////////////
+        //[HttpPost]
+        //public async Task<IActionResult> AddFile(IFormFile uploadedFile, Movie movie)
+        //{
+        //    if (uploadedFile != null && movie != null)
+        //    {
+        //        string fileName = Path.GetFileName(uploadedFile.FileName);
+        //        string filePath = Path.Combine(_webHostEnvironment.WebRootPath, "Image", fileName);
+        //        using (var fileStream = new FileStream(filePath, FileMode.Create))
+        //        {
+        //            await uploadedFile.CopyToAsync(fileStream);
+        //        }
+        //        movie.PosterPath = "/Image/" + fileName;
 
-		// Get запрос на Edit
-		public async Task<IActionResult>Edit(int? id)
+        //        _movieContext.Movies.Add(movie);
+        //        await _movieContext.SaveChangesAsync();
+
+        //        //string posterPath = "/Image/" + uploadedFile.FileName;
+
+        //        //using (var fileStream = new FileStream(_webHostEnvironment.WebRootPath + posterPath, FileMode.Create))
+        //        //{
+        //        //    await uploadedFile.CopyToAsync(fileStream);
+        //        //}
+
+        //        //Movie posterFile = new Movie { Title = movie, PosterPath = posterPath };
+        //        //_movieContext.Movies.Add(posterFile);
+        //        //_movieContext.SaveChanges();
+        //    }
+        //    return RedirectToAction("Index");
+        //}
+
+
+
+        //// Post запрос на Create
+        //[HttpPost] // получение данных от клиента, при нажатии СОХРАНИТЬ данные сохраняются в БД
+        //[ValidateAntiForgeryToken] // проверка данных перед тем как они отправятся в базу данных
+
+        //// Bind - инициализация полей объекта, происходит напрямую через форму html
+        //public async Task<IActionResult> Create([Bind("Id,Title,Director,Genre,ReleaseYear,PosterPath,Description")] Movie movie, IFormFile posterFile)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        if (posterFile != null && posterFile.Length > 0)
+        //        {
+        //            string fileName = Path.GetFileName(posterFile.FileName);
+        //            string filePath = Path.Combine(_webHostEnvironment.WebRootPath, "Image", fileName);
+
+        //            using (var fileStream = new FileStream(filePath, FileMode.Create))
+        //            {
+        //                await posterFile.CopyToAsync(fileStream);
+        //            }
+        //        }
+
+        //        _movieContext.Add(movie);
+        //        await _movieContext.SaveChangesAsync(); // синхронизация с БД
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(movie);
+        //}
+
+        // !!! c этим методом сохраняется и отображается новый фильм, но нет постера !!!
+        // Bind - инициализация полей объекта, происходит напрямую через форму html
+        //[HttpPost]
+        //public async Task<IActionResult> Create([Bind("Id,Title,Director,Genre,ReleaseYear,PosterPath,Description")] Movie movie)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _movieContext.Add(movie);
+        //        await _movieContext.SaveChangesAsync(); // синхронизация с БД
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(movie);
+        //}
+
+
+        ///////////////////////////////////////////////////////////////////////////////////////////
+
+        // Get запрос на Edit
+        public async Task<IActionResult>Edit(int? id)
         {
             if(id == null)
             {
