65a1edeac1a2442ec51c8ebcaa2ba0cd342e05c2
Petr Zalizko <pzalizko@gmail.com>
2024-05-15 11:26:21 +0300

    Добавлены возможности РЕДАКТИРОВАТЬ и ДОБАВИТЬ фильм (методы CREATE & EDIT). Можно добавлять картинки не только с wwwroot, при этом картинки записываются в папку Image и генерируются новые названия для фото в папке

--- StudentsMVC/Controllers/MovieController.cs
+++ StudentsMVC/Controllers/MovieController.cs
@@ -24,7 +24,7 @@ namespace StudentsMVC.Controllers
 
 		///////////////////////////////////////////////////////////////////////////////////////////
 
-		// GET запрос
+		// GET запрос для отображения всех фильмов в списке
 		public async Task<IActionResult> Index()
         {
             return View( await _movieContext.Movies.ToListAsync());
@@ -32,7 +32,7 @@ namespace StudentsMVC.Controllers
 
 		///////////////////////////////////////////////////////////////////////////////////////////
 
-		// GET
+		// GET запрос для отображения деталей конкретного выбранного фильма
 		public async Task<IActionResult>Details(int? id)
         {
             if(id == null)
@@ -61,35 +61,40 @@ namespace StudentsMVC.Controllers
 		}
         */
 
+        // GET запрос для отображения формы создания нового фильма
         [HttpGet]
         public IActionResult Create()
         {
             return View();
         }
-
+                
+        // POST запрос для создания нового фильма в списке
+        // Bind - инициализация полей объекта, происходит напрямую через форму html
         [HttpPost]
-        public async Task<IActionResult> Create([Bind("Id,Title,Director,Genre,ReleaseYear,Description")] Movie movie, IFormFile posterFile)
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Director,Genre,ReleaseYear,Description")] Movie movie, IFormFile uploadedFile)
         {
             if (ModelState.IsValid)
             {
-                if (posterFile != null && posterFile.Length > 0)
+                if (uploadedFile != null && uploadedFile.Length > 0)
                 {
-                    string fileName = Path.GetFileName(posterFile.FileName);
-                    string filePath = Path.Combine(_webHostEnvironment.WebRootPath, "Image", fileName);
+                    string uploadedFolder = Path.Combine(_webHostEnvironment.WebRootPath, "Image");
+                    string newFileNameGenerated = Guid.NewGuid().ToString() + "_" + uploadedFile.FileName;
+                    string filePath=Path.Combine(uploadedFolder, newFileNameGenerated);
 
                     using (var fileStream = new FileStream(filePath, FileMode.Create))
                     {
-                        await posterFile.CopyToAsync(fileStream);
+                        await uploadedFile.CopyToAsync(fileStream);
                     }
-                    movie.PosterPath = "/Image/" + fileName;
+                    movie.PosterPath="/Image/"+newFileNameGenerated;
                 }
                 _movieContext.Add(movie);
-                await _movieContext.SaveChangesAsync();
-                return RedirectToAction("Index");
+                await _movieContext.SaveChangesAsync(); // синхронизация с БД
+                return RedirectToAction(nameof(Index));
             }
             return View(movie);
         }
-        
+
 
         //[HttpPost]
         //public async Task<IActionResult> AddFile(IFormFile uploadedFile, Movie movie)
@@ -167,7 +172,8 @@ namespace StudentsMVC.Controllers
 
         ///////////////////////////////////////////////////////////////////////////////////////////
 
-        // Get запрос на Edit
+        // GET запрос на отображение формы редактирования фильма
+        [HttpGet]
         public async Task<IActionResult>Edit(int? id)
         {
             if(id == null)
@@ -185,40 +191,81 @@ namespace StudentsMVC.Controllers
             return View(movie);
         }
 
+
+        // POST запрос для редактирования фильма
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult>Edit(int id, [Bind("Id,Title,Director,Genre,ReleaseYear,PosterPath,Description")] Movie movie)
+        public async Task<IActionResult> Edit(int Id, [Bind("Id,Title,Director,Genre,ReleaseYear,Description")] Movie movie, IFormFile uploadedFile)
         {
-            if(id != movie.Id)
+            if (Id != movie.Id)
             {
                 return NotFound();
             }
 
             if (ModelState.IsValid)
             {
-                try
+                if (uploadedFile != null && uploadedFile.Length>0)
                 {
-                    _movieContext.Update(movie);
-                    await _movieContext.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!MovieExists(movie.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }					
-				}
-				return RedirectToAction(nameof(Index));
-			}
-            return View(movie);
-        }
+                    string uploadedFolder = Path.Combine(_webHostEnvironment.WebRootPath, "Image");
+                    string newFileNameGenerated = Guid.NewGuid().ToString() + "_" + uploadedFile.FileName;
+                    string filePath=Path.Combine(uploadedFolder, newFileNameGenerated);
 
-		///////////////////////////////////////////////////////////////////////////////////////////
+                    using (var fileStream=new FileStream(filePath, FileMode.Create))
+                    {
+                        await uploadedFile.CopyToAsync(fileStream);
+                    }
+
+                    movie.PosterPath = "/Image/" + newFileNameGenerated;
+                }
+
+                _movieContext.Update(movie);
+                await _movieContext.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+			return View(movie);
+		}
+
+
+        //     // !!! этот метод по редактированию работает, но частично, загружаются фото только из папки wwwroot
+        //     [HttpPost]
+        //     [ValidateAntiForgeryToken]
+        //     public async Task<IActionResult>Edit(int id, [Bind("Id,Title,Director,Genre,ReleaseYear,PosterPath,Description")] Movie movie)
+        //     {
+        //         string fullPath = "/Image/" + movie.PosterPath;
+        //         movie.PosterPath = fullPath;
+
+        //         if(id != movie.Id)
+        //         {
+        //             return NotFound();
+        //         }
+
+        //         if (ModelState.IsValid)
+        //         {
+        //             try
+        //             {
+        //                 _movieContext.Update(movie);
+        //                 await _movieContext.SaveChangesAsync();
+        //             }
+        //             catch (DbUpdateConcurrencyException)
+        //             {
+        //                 if (!MovieExists(movie.Id))
+        //                 {
+        //                     return NotFound();
+        //                 }
+        //                 else
+        //                 {
+        //                     throw;
+        //                 }					
+        //	}
+        //	return RedirectToAction(nameof(Index));
+        //}
+        //         return View(movie);
+        //     }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////
         
+        // GET запрос формы для удаления фильма
+        [HttpGet]        
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -235,6 +282,7 @@ namespace StudentsMVC.Controllers
             return View(movie);
         }
 
+        // POST запрос для удаления фильма
         [HttpPost,ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id) // почему пропало подчёркивание когда я написал return RedirectToAction(nameof(Index));
@@ -254,6 +302,7 @@ namespace StudentsMVC.Controllers
 
 		///////////////////////////////////////////////////////////////////////////////////////////
 
+        // Вспомогательный метод для проверки существования фильма
 		private bool MovieExists(int id)
         {
             return _movieContext.Movies.Any(item => item.Id == id);
